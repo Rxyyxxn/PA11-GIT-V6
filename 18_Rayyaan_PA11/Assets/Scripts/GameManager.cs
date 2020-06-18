39df799617207ebf59817b409e0db3e73cfe39ca
Rxyyxxn <rayyaanputra@gmail.com>
2020-06-18 17:06:25 +0800

    UPDATED BY Rayyaan

---
+++ 18_Rayyaan_PA11/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float points;
+    public Text Point_txt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    Point_txt.text = "Points: " + points;
+        
+    }
+    public void GameScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Hit"))
+        {
+            points++;
+        }
+    }
+
+}
